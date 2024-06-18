5935104bb6a2fa00e4bb76ce416cc9b3793ae80d
JimmyLam9758 <JimmyLam9758@gmail.com>
2024-06-18 20:08:44 +1000

    feat: server with mongoose connection initialised

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const { app } = require("./server.js");
+
+const { connect } = require("./utils/database");
+
+app.listen(3000, () => {
+    connect();
+    console.log("Server running on port 3000");
+});
